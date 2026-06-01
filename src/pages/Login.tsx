00fa9b11156814ed4c98a1ba6b0fa1bb27c99b2a
luislunaec <luisandreluna01@hotmail.com>
2026-06-01 07:55:20 -0500

    feat(KAN-47): setup react-router-dom and auth views skeleton

---
+++ src/pages/Login.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-50">
+      <h1 className="text-3xl font-bold text-gray-800">Pantalla de Login (KAN-46)</h1>
+    </div>
+  );
+}
---
+++ src/pages/Register.tsx
@@ -0,0 +1,7 @@
+export default function Register() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-50">
+      <h1 className="text-3xl font-bold text-gray-800">Pantalla de Registro (KAN-45)</h1>
+    </div>
+  );
+}
