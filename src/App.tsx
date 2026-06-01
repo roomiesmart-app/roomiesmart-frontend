684da7813c1a1df73d17aea4945f551af66192e5
luislunaec <luisandreluna01@hotmail.com>
2026-06-01 10:15:48 -0500

    feat: complete KAN-45 onboarding flow with RegisterStep 2, 3, 4 and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import RegisterStep2 from './pages/RegisterStep2'; 
+import RegisterStep3 from './pages/RegisterStep3';
+import RegisterStep4 from './pages/RegisterStep4';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Redirección automática de la raíz al login */}
         <Route path="/" element={<Navigate to="/login" replace />} />
-        
-        {/* Nuestras rutas principales */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        {/* Agregamos la ruta del paso 2 */}
+        <Route path="/register/step-2" element={<RegisterStep2 />} />
+        <Route path='register/step-3' element={<RegisterStep3/>}></Route>
+        <Route path='register/step-4' element={<RegisterStep4/>}></Route>
       </Routes>
     </BrowserRouter>
   );
 }
-
 export default App;
