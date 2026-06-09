44a7ebd4b28d0c5c1af03b1a1192984879105887
Luis Luna <luisandreluna01@hotmail.com>
2026-06-09 09:13:37 -0500

    Revert "Feature/kan 48 integration end to end"

--- src/app/routes/constant.ts
+++
@@ -1,6 +0,0 @@
-export const ONBOARDING_ROUTES = {
-  IDENTITY: '/register/step-1',
-  LIFESTYLE: '/register/step-2',
-  SOCIAL: '/register/step-3',
-  FINANCIAL: '/register/step-4',
-};
--- src/shared/utils/validationHelper.ts
+++
@@ -1,3 +0,0 @@
-export const hasErrors = <T extends object>(errors: T): boolean => {
-  return Object.keys(errors).length > 0;
-};
