d71665f312372f39bbc02c64c6cd2030332eb09e
Maebara505 <darvoxtupapi@gmail.com>
2026-07-05 09:51:09 -0500

    publicar de nuevo

--- src/app/routes/App.tsx
+++ src/app/routes/App.tsx
@@ -37,88 +37,91 @@ function App() {
         <RoomieProvider>
           <Routes>
             <Route path="/" element={<Navigate to="/dispatcher" replace />} />
-          <Route
-            path="/dispatcher"
-            element={
-              <ProtectedRoute>
-                <AuthDispatcher />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              <PublicRoute>
-                <Login />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="/onboarding/*"
-            element={
-              <ProtectedRoute>
-                <OnboardingProvider>
-                  <Routes>
-                    <Route element={<OnboardingLayout />}>
-                      <Route index element={<Navigate to="step-1" replace />} />
-                      <Route
-                        path="step-1"
-                        element={<OnboardingIdentityPage />}
-                      />
-                      <Route
-                        path="step-2"
-                        element={<OnboardingLifestylePage />}
-                      />
-                      <Route
-                        path="step-3"
-                        element={<OnboardingSocialProfilePage />}
-                      />
-                      <Route
-                        path="step-4"
-                        element={<OnboardingFinancialExpectationPage />}
-                      />
-                    </Route>
-                  </Routes>
-                </OnboardingProvider>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <WelcomePage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/matchmaking"
-            element={
-              <ProtectedRoute>
-                <MatchmakingDashboardPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/publish-department"
-            element={
-              <ProtectedRoute>
-                <PublishDepartmentPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/finanzas"
-            element={
-              <ProtectedRoute>
-                <MainLayout>
-                  <FinanceDashboardPage />
-                </MainLayout>
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </RoomieProvider>
+            <Route
+              path="/dispatcher"
+              element={
+                <ProtectedRoute>
+                  <AuthDispatcher />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <PublicRoute>
+                  <Login />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/onboarding/*"
+              element={
+                <ProtectedRoute>
+                  <OnboardingProvider>
+                    <Routes>
+                      <Route element={<OnboardingLayout />}>
+                        <Route
+                          index
+                          element={<Navigate to="step-1" replace />}
+                        />
+                        <Route
+                          path="step-1"
+                          element={<OnboardingIdentityPage />}
+                        />
+                        <Route
+                          path="step-2"
+                          element={<OnboardingLifestylePage />}
+                        />
+                        <Route
+                          path="step-3"
+                          element={<OnboardingSocialProfilePage />}
+                        />
+                        <Route
+                          path="step-4"
+                          element={<OnboardingFinancialExpectationPage />}
+                        />
+                      </Route>
+                    </Routes>
+                  </OnboardingProvider>
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <WelcomePage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/matchmaking"
+              element={
+                <ProtectedRoute>
+                  <MatchmakingDashboardPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/publish-department"
+              element={
+                <ProtectedRoute>
+                  <PublishDepartmentPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/finanzas"
+              element={
+                <ProtectedRoute>
+                  <MainLayout>
+                    <FinanceDashboardPage />
+                  </MainLayout>
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </RoomieProvider>
       </AxiosInterceptor>
     </BrowserRouter>
   );
